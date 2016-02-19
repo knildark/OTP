c442534922565bc3b486cad1b9d77300807ad239
Plailect <plailect@gmail.com>
2016-02-19 17:16:28 -0500

    Update LinuxOrMac.py

--- New_3DS_Spider_U/Section_II/Bricked/LinuxOrMac.py
+++ New_3DS_Spider_U/Section_II/Bricked/LinuxOrMac.py
@@ -1,12 +1,12 @@
 import mmap
 import os
 
-os.system('resource/3DSFAT16tool -d -n emuNAND_bricked.bin ctr.bin ../../Section_I/Backup/nand.fat16.xorpad')
-os.system('resource/3DSFAT16tool -i -o emuNAND_bricked.bin ctr.bin ../../Section_I/Backup/nand.fat16.0x4.xorpad')
+os.system('Resource/3DSFAT16tool -d -n emuNAND_bricked.bin ctr.bin ../../Section_I/Backup/nand.fat16.xorpad')
+os.system('Resource/3DSFAT16tool -i -o emuNAND_bricked.bin ctr.bin ../../Section_I/Backup/nand.fat16.0x4.xorpad')
 os.system('rm ctr.bin')
 
 f1 = open("emuNAND_bricked.bin", "r+b")
-f2 = open("resource/NCSD_header_o3ds.bin", "r+b")
+f2 = open("Resource/NCSD_header_o3ds.bin", "r+b")
 
 m1 = mmap.mmap(f1.fileno(), 0x200)
 m2 = mmap.mmap(f2.fileno(), 0x200)
