7b666cde55a7e5d105fa7256cacd22aaa4b35772
Plailect <Plailect@gmail.com>
2016-02-26 17:16:02 -0500

    switched to bash/batch from python

---
+++ New_3DS_Cubic_E/Section_II/Bricked/Mac.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+if [ ! -f ../../Section_I/Backup/nand.fat16.xorpad ]; then
+    echo "Could not find nand.fat16.xorpad in Section_I/Backup!"
+    exit 1
+fi
+
+echo "Found nand.fat16.xorpad!"
+
+if [ ! -f ../../Section_I/Backup/nand.fat16.xorpad ]; then
+    echo "Could not find nand.fat16.0x4.xorpad in Section_I/Backup!"
+    exit 1
+fi
+
+echo "Found nand.fat16.0x4.xorpad!"
+
+if [ -f Unbricked/sysNAND.bin ]; then
+    echo "Removing previous unbrick..."
+    rm Unbricked/sysNAND.bin
+    rmdir Unbricked
+fi
+
+mv emuNAND_bricked.bin Resource/
+cd Resource/
+
+echo "Copying emuNAND, please wait..."
+cp emuNAND_bricked.bin sysNAND.bin
+
+./3DSFAT16tool_Darwin -d -n sysNAND.bin ctr.bin ../../../Section_I/Backup/nand.fat16.xorpad
+./3DSFAT16tool_Darwin -i -o sysNAND.bin ctr.bin ../../../Section_I/Backup/nand.fat16.0x4.xorpad
+
+dd conv=notrunc if=NCSD_header_o3ds.bin of=sysNAND.bin
+
+cd ..
+
+mkdir Unbricked
+
+mv Resource/sysNAND.bin Unbricked/sysNAND.bin
+mv Resource/emuNAND_bricked.bin .
---
+++ New_3DS_Cubic_E/Section_II/Bricked/Windows.bat
@@ -0,0 +1,44 @@
+@echo off
+
+if exist ../../Section_I/Backup/nand.fat16.xorpad (
+    echo "Found nand.fat16.xorpad!"
+) else (
+    echo "Could not find nand.fat16.xorpad in Section_I/Backup!"
+    EXIT /B
+)
+
+if exist ../../Section_I/Backup/nand.fat16.0x4.xorpad (
+    echo "Found nand.fat16.0x4.xorpad!"
+) else (
+    echo "Could not find nand.fat16.0x4.xorpad in Section_I/Backup!"
+    EXIT /B
+)
+
+if exist Unbricked/sysNAND.bin (
+    echo "Removing previous unbrick..."
+    del Unbricked/sysNAND.bin
+    rmdir Unbricked
+) else (
+    echo "No previous unbrick found..."
+)
+
+
+move emuNAND_bricked.bin Resource/
+cd Resource/
+
+echo "Copying emuNAND, please wait..."
+copy emuNAND_bricked.bin sysNAND.bin
+
+3DSFAT16tool.exe -d -n sysNAND.bin ctr.bin ../../../Section_I/Backup/nand.fat16.xorpad
+3DSFAT16tool.exe -i -o sysNAND.bin ctr.bin ../../../Section_I/Backup/nand.fat16.0x4.xorpad
+
+dd.exe conv=notrunc if=NCSD_header_o3ds.bin of=sysNAND.bin
+
+cd ..
+
+mkdir Unbricked
+
+move Resource/sysNAND.bin Unbricked/sysNAND.bin
+move Resource/emuNAND_bricked.bin Unbricked/sysNAND.bin
+
+pause
